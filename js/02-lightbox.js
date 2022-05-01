3775fdd54770cb5cb72d05556635f1f51689871c
Olena Pohybil <itlentus@gmail.com>
2022-05-01 21:53:37 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const createGallery = () => {
+    galleryItems.map(({ preview, original, description }) => {
+
+    const galleryItem = `<div class="gallery__unit">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`
+        
+gallery.insertAdjacentHTML('afterbegin', galleryItem);
+    })
+}
+
+createGallery();
+
+let galleryLight = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
